07422a7f9d7f2ee440df747870f6c6ef753f6551
Kyle Gordy <kyletgordy@gmail.com>
2024-04-23 17:16:11 -0400

    First pass at a basic mobile menu toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  config: { applyBaseStyles: false },
+  integrations: [alpinejs(), tailwind()],
+  config: {
+    applyBaseStyles: false,
+  },
 });
