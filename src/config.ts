d35fd73d154223be74220c9ce0544584f2bc4b8e
Kyle Gordy <kyletgordy@gmail.com>
2024-05-05 19:13:49 -0400

    Bump fundraiser amount

--- src/config.ts
+++ src/config.ts
@@ -11,5 +11,5 @@ export const patrion_url = "https://www.patreon.com/poststroke";
 
 // Fundraiser
 export const fundraiser_goal_amount = "10,000";
-export const fundraiser_raised_amount = "867";
-export const fundraiser_raised_percent = "8.6";
+export const fundraiser_raised_amount = "1,142";
+export const fundraiser_raised_percent = "11.4";
